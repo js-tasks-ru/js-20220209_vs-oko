89e5185fa169da2311ed204fb41c1d4074f1265d
Aleksei Belousov <vs.oko@rambler.ru>
2022-02-15 22:46:45 +0300

    Задачи по модулю 1 (по задаче 1 с localeCompare() не уверен что учитывает языки корректно, но тесты проходит локально)

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -1,9 +1,11 @@
 /**
- * sortStrings - sorts array of string by two criteria "asc" or "desc"
- * @param {string[]} arr - the array of strings
- * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
- * @returns {string[]}
- */
+   * sortStrings - sorts array of string by two criteria "asc" or "desc"
+   * @param {string[]} arr - the array of strings
+   * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
+   * @returns {string[]}
+   */
 export function sortStrings(arr, param = 'asc') {
-
+  // eslint-disable-next-line no-nested-ternary
+  return param === 'asc' ? [...arr].sort((a, b) => a.localeCompare(b, ['ru-u-kf-upper', 'en-u-kf-upper']))
+    : param === 'desc' ? [...arr].sort((a, b) => b.localeCompare(a, ['ru-u-kf-upper', 'en-u-kf-upper'])) : '';
 }
